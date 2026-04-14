118bf7355cfa523b64f123fa5ef5a8cf7c6da288
stuckin2004 <stuck_in2004@proton.me>
2026-04-14 20:36:06 -0230

    Create getdeps.cmd

---
+++ getdeps.cmd
@@ -0,0 +1,28 @@
+rem This assumes that you've got git installed on your system already.
+rem If not, you can get Git 2.10 for Windows here vv
+rem https://github.com/git-for-windows/git/releases/tag/v2.10.0.windows.1
+rem
+
+@echo off
+setlocal
+cls
+
+set "DEPS_DIR=%~dp0deps"
+
+if exist "%DEPS_DIR%\" (
+  echo Dependencies folder already exists, skipping.
+  exit /b 0
+)
+
+echo Preparing to clone dependencies...
+ping -n 2 127.0.0.1 >nul
+md "%DEPS_DIR%"
+pushd "%DEPS_DIR%"
+
+echo Cloning miniaudio...
+git clone --depth 1 https://github.com/mackron/miniaudio.git
+
+echo Cloning Lua 5.4.7...
+git clone --depth 1 --branch v5.2.3 https://github.com/lua/lua.git
+
+popd
