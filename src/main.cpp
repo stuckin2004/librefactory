877903177e16a9158d3df78602683240c36a128b
stuckin2004 <stuck_in2004@proton.me>
2026-04-15 09:49:21 -0230

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 	#define WIN32_LEAN_AND_MEAN
 #endif
 
+#pragma comment(lib, "ddraw.lib")
+#pragma comment(lib, "dxguid.lib")
+
 #include <windows.h>
 
 /* entrypoint file, don't use main, only WinMain() ! */
-
 int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdLine, int cmdShow)
 {
 	return MessageBox(NULL, "Hello Windows!", "Caption", 0);
